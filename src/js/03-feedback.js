3e92528c7f3d9694d39a1e98b5a9ed5eb758ccd3
Дмитрий Мариненко <dimkamar69@gmail.com>
2023-09-22 18:48:00 +0300

    s

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,6 @@ const LOCAL_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
 form = document.querySelector('.feedback-form');
-
 form.addEventListener('input', throttle(storageFormData, 500));
 form.addEventListener('submit', onFormSubmit);
 
